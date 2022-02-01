03c55b93fca7f923596c3ad215c383bf316e0a7e
Gimex10 <decapajo@gmail.com>
2022-02-01 23:53:08 +0300

    Template commit

---
+++ ecommerce/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def store(request):
+
+    context = {}
+
+    return render(request, 'ecommerce/store.html', context)
+
+
+def checkout(request):
+
+    context = {}
+
+    return render(request, 'ecommerce/checkout.html', context)
+
+
+def cart(request):
+
+    context = {}
+
+    return render(request, 'ecommerce/cart.html', context)
+
+
+def home(request):
+
+    context = {}
+
+    return render(request, 'ecommerce/main.html', context)
