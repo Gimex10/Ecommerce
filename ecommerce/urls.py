70912f76f2468062b39fdff9f5e8cf3e491fd1ac
Gimex10 <decapajo@gmail.com>
2022-04-05 04:36:34 +0300

    models-update

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('store', views.store, name="store"),
-    path('cart', views.cart, name="cart"),
-    path('checkout', views.checkout, name="checkout"),
+    path('store/', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('home/', views.home, name="home"),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,25 +1,44 @@
 from django.shortcuts import render
 
+
+from .models import *
 # Create your views here.
 
 
 def store(request):
 
-    context = {}
+    products = Product.objects.all()
+
+    context = {'products': products}
 
     return render(request, 'ecommerce/store.html', context)
 
 
 def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
 
-    context = {}
+    context = {'items':items, 'order':order}
 
     return render(request, 'ecommerce/checkout.html', context)
 
 
 def cart(request):
 
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
 
     return render(request, 'ecommerce/cart.html', context)
 
@@ -28,4 +47,4 @@ def home(request):
 
     context = {}
 
-    return render(request, 'ecommerce/main.html', context)
+    return render(request, 'ecommerce/index.html', context)
